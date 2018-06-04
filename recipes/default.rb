342d90a5cf30695022b0e59c2700cf3f1c418744
hieunba <hi3unb@gmail.com>
2018-06-05 04:14:20 +0700

    Add checksum

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,6 +5,7 @@
 # Copyright:: 2018, Nghiem Ba Hieu, Apache License, Version 2.0.
 windows_package 'Firefox Installer' do
   source 'https://storage.googleapis.com/chef_files/Firefox.exe'
+  checksum 'c7625a033ac40cd96210e68ff02e114a5fdd3b08de345f98ea8f221c4ebc02c2'
   installer_type :custom
   options '/S'
 end
