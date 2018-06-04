fda3f028b00171078d9cf2923570590512940be8
hieunba <hi3unb@gmail.com>
2018-06-05 03:52:03 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# firefox
+
+This cookbook to install Firefox
+
+## Supports
+
+Windows
+
+## References
+
+About deploying Firefox, please refer to [Deploy:Deploy Firefox](https://wiki.mozilla.org/Deployment:Deploying_Firefox#Deploying_Firefox)
---
+++ recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook:: firefox
+# Recipe:: default
+#
+# Copyright:: 2018, Nghiem Ba Hieu, Apache License, Version 2.0.
+windows_package 'Firefox Installer' do
+  source 'https://storage.googleapis.com/chef_files/Firefox.exe'
+  installer_type :custom
+  options '/Q'
+end
